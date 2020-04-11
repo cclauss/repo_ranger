6c940664a0b160673af65d57000f7131cf263bea
cclauss <cclauss@me.com>
2020-04-11 18:31:59 +0200

    Need a new name

--- repo_ranger.py
+++ repo_ranger.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
-from subprocess import PIPE, run
+import logging
+from subprocess import CompletedProcess, PIPE, run
+from Typing import List
+
+logger = logging.getLogger(__name__)
+
+
+def run_cmd(cmd: List[str]) -> CompletedProcess:
+    if isinstance(cmd, str) and "" in cmd:
+        cmd = cmd.split()
+    return run(cmd, text=True, stdout=PIPE, stderr=PIPE)
+
+
+def run_cmd_print(cmd: List[str]) -> CompletedProcess:
+    completed_process = run_cmd(cmd)
+    print(f"{completed_process.returncode=}")
+    if completed_process.returncode:
+        print("\n".join(completed_process.stderr.splitlines()))
+    else:
+        print("\n".join(completed_process.stdout.splitlines()))
+    return completed_process
 
 
 if __name__ == "__main__":
@@ -14,10 +34,12 @@ if __name__ == "__main__":
             repo = input("Or leave blank to quit: ").strip()
             if not repo:
                 break
-            print(repo)
-            result = run(["ls", "-lha"], text=True, stdout=PIPE, stderr=PIPE)
-            print(f"{result.returncode=}")
-            if result.returncode:
-                print(f"{result.stderr=}")
-            else:
-                print(f"{result.stdout=}")
+            print(f"{repo=}")
+            out_file.write(f"{repo=}\n")
+            logger.debug(f"{repo=} --> debug")
+            logger.info(f"{repo=} --> info")
+            logger.warning(f"{repo=} --> warning")
+
+            result = run_cmd_print(["ls", "-la"])
+            result = run_cmd_print(["ls", "-Fla"])
+            result = run_cmd_print(["ls", "-lha"])            
